856bb5475b717d168678ca1ac653bc3e9d3e8a89
Xiaolu Bai <icebunnydoll@gmail.com>
2017-02-22 03:25:19 -0800

    Added input box and subscriptions

--- imports/ui/Messages.js
+++ imports/ui/Messages.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 
 export default class MessageView extends Component {
+  formatTime(time) {
+    return moment(time).format("h:mm A");
+  }
   showMessages(data){
     return data.map((currentMessage, index) => {
       return (
         <ul key={index}>
-          <li>{currentMessage.text}</li>
+          <li>{this.formatTime(currentMessage.createdAt.toString())}
+            <label> - {currentMessage.userName}</label>
+            <label className="pull-right">{currentMessage.text}</label>
+          </li>
         </ul>
       )
     })
